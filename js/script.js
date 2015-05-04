0d1da33750d3a92bbf1ba3481b6d6ed1a105e4e2
AmandaGentle <gentleamanda21@gmail.com>
2015-05-04 09:01:42 -0500

    CSS nav changes

--- js/script.js
+++ js/script.js
@@ -6,25 +6,83 @@ $(document).ready(function() {
 		$('body').animate({scrollTop: 0}, 900); // you can adjust "900" to change speed of scroll
 	});
 
-	// auto scroll to "ONE"
+	// auto scroll to "Vanilla"
 	$("#go_van").click(function() {  // #go_one is the button that starts the scroll
     	$('html, body').animate({
         	scrollTop: $("#van_link").offset().top // #one is destination of scroll
     	}, 900);  // you can adjust "900" to change speed of scroll
 	});
 
-	// auto scroll to "TWO"
-	$("#go_two").click(function() {
+	// auto scroll to "Cookies 'n Cream"
+	$("#go_oreo").click(function() {
     	$('html, body').animate({
-        	scrollTop: $("#two").offset().top
+        	scrollTop: $("#oreo_link").offset().top
     	}, 900);
 	});
 
-	// auto scroll to "THREE"
-	$("#go_three").click(function() {
+	// auto scroll to "Cookie Dough"
+	$("#go_cookie").click(function() {
     	$('html, body').animate({
-        	scrollTop: $("#three").offset().top
+        	scrollTop: $("#cookie_start").offset().top
     	}, 900);
+	});
+	
+		// auto scroll to "Mint Chip"
+	$("#go_mint").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#mint_link").offset().top
+    	}, 900);
+	});
+	
+			// auto scroll to "Strawberry"
+	$("#go_straw").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#straw_start").offset().top
+    	}, 900);
+	});
+	
+				// auto scroll to "Neapolitan"
+	$("#go_nea").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#nea_start").offset().top
+    	}, 900);
+	});
+	
+					// auto scroll to "Coffee"
+	$("#go_coffee").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#coffee_start").offset().top
+    	}, 900);
+	});
+	
+						// auto scroll to "Rocky Road"
+	$("#go_rocky").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#rocky_start").offset().top
+    	}, 900);
+	});
+	
+							// auto scroll to "Chocolate"
+	$("#go_choco").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#choco_start").offset().top
+    	}, 900);
+	});
+	
+								// auto scroll to "Chocolate"
+	$("#go_method").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#info_start").offset().top
+    	}, 900);
+	});
+	
+	
+	$('#nav').mouseenter(function() {
+		$(this).animate({right: "0px"});
+	});
+	
+	$('#nav').mouseleave(function() {
+		$(this).animate({right: "-200px"});
 	});
 	
 });
